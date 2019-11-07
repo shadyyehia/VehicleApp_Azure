d81a3499fc661b6cd0a13343c54d3d4006cde7ec
Shady Yehia <shady.yahia@itworx.com>
2019-11-07 21:15:26 +0200

    updated libraries. made server test fails

--- UnitTest_VehiclesWebAPI/VehicleControllerTest.cs
+++ UnitTest_VehiclesWebAPI/VehicleControllerTest.cs
@@ -1,13 +1,10 @@
 using Vehicle_WebAPI.Controllers;
 using Vehicle_WebAPI.Helpers;
-using Vehicle_WebAPI.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Moq;
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTest_VehiclesWebAPI
 {
@@ -17,10 +14,13 @@ namespace UnitTest_VehiclesWebAPI
         private readonly Mock<ITimerManager> _mockTimeManager;
         private readonly Mock<IHubContext<VehicleHub,IVehicleHub>> _mockVehicleHub;
         private readonly VehicleController _controller;
-
+        int savedPeriod;
         public VehicleControllerTest() {
             _mockDataManager = new Mock<IDataManager>();
             _mockTimeManager = new Mock<ITimerManager>();
+          
+            _mockTimeManager.Setup(w => w.Configure(It.IsAny<Action>(), It.IsAny<int>(), It.IsAny<int>()))
+                .Callback<Action, int, int>((i, obj, period) => savedPeriod = period);
             _mockVehicleHub = new Mock<IHubContext<VehicleHub, IVehicleHub>>();
             _controller = new VehicleController(_mockVehicleHub.Object, _mockDataManager.Object,_mockTimeManager.Object);
         }
@@ -31,11 +31,13 @@ namespace UnitTest_VehiclesWebAPI
            Assert.IsAssignableFrom<IActionResult>(result);
         }
         [Fact]
-        public void Monitor_CheckSignalRPeriod3Seconds()
+        public void Monitor_CheckSignalRPeriod60Seconds()
         {
             var result = _controller.Monitor();
-            _mockTimeManager.Verify(m => m.Configure(It.IsAny<Action>(), It.IsAny<int>(), 3000), Times.Once());
-            
+            _mockTimeManager.Verify(m => m.Configure(It.IsAny<Action>(), It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+            // Assert about saveObject
+            Assert.Equal(50000, savedPeriod);
+
         }
        
 
